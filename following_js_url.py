b46b86d3f8a5db0f931c84294ebf81b7f5bea6b8
waremad <mycarline5@gmail.com>
2024-12-15 17:54:24 +0900

    実装

--- following_js_url.py
+++ following_js_url.py
@@ -1,4 +1,31 @@
 #
-def readjs(self):
+from os import path
+
+
+def readjs(self):#.jsファイルを読み込む
     with open(self+".js") as f:
         return f.read()
+
+def deln(self):#\nを消す
+    self = self.split("\n")
+    self = "".join(self)
+    return self
+
+def picurl(self):#httpが入るのを抽出
+    self = self.split("\"")
+    out = []
+    for i in self:
+        if "http" == i[:4]:
+            out.append(i)
+    return out
+
+path = input("???.js:")
+txt = readjs(path)
+ls = picurl(txt)
+path_w = 'output.txt'
+with open(path_w, mode='w') as f:
+        f.write("")
+
+for i in ls:
+    with open(path_w, mode='a') as f:
+        f.write(i+"\n")
--- following_js_url_test.py
+++ following_js_url_test.py
@@ -4,3 +4,12 @@ def test_readjs():
     assert readjs("test1") == "TEST1"
     assert readjs("test2") == "test2\n22"
 
+def test_deln():
+    assert deln("") == ""
+    assert deln("abc") == "abc"
+    assert deln("qed\nabo") == "qedabo"
+    assert deln("qed\nabo\n\nqed\nabo") == "qedaboqedabo"
+
+def test_picurl():
+    assert picurl("ふぃいあんヴぃ\"http:test\"daghjpe") == ["http:test"]
+
